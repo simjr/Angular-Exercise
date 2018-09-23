0c907aee8de3f9071406284aa73684b7dd9d9ab8
Simon Rojas <simonjrojas@hotmail.es>
2018-09-23 17:41:05 -0400

    Fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+	//Recordar Orden de rutas (SI IMPORTA):
+	// Primero las rutas estáticas, luego la predeterminada, y de último el comodín
+	{ path: 'home', component: HomeComponent },
 	{ path: '', redirectTo: 'home', pathMatch: 'full'  },
-	{ path: 'home', component: HomeComponent }
+	{ path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
